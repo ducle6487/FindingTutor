2d41883bf32ca594dfc407fbeb0ef42f9c3b01eb
AnhDuc <ducle6487@gmail.com>
2021-04-06 17:08:10 +0700

    install project

---
+++ FindingTutor/Models/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FindingTutor.Models
+{
+    public class DBConnection
+    {
+        private string strCon;
+
+        public DBConnection()
+        {
+            strCon = ConfigurationManager.ConnectionStrings["FindingTutorConnectionString"].ConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(strCon);
+        }
+    }
+}
