3b9bb1dc6360313d0ab967e6bc62a966e2e53bd7
Pham Minh Sang <harley20005@gmail.com>
2021-04-07 11:56:19 +0700

    thsy doi controller

--- FindingTutor/Controllers/HomeController.cs
+++ FindingTutor/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace FindingTutor.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            string s;
             return View();
         }
 
